afa2fb9b7d85ff5504b6a0ac1c6fa82c567c47b8
Serkan BOYRACI <serkanboyraci@gmail.com>
2022-09-07 14:41:08 +0300

    Initial Commit

---
+++ TimerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerProject
+//
+//  Created by Ali serkan Boyracı  on 7.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
